a6880d9539924fe813f543d0a83159f356792211
Damien Le Dantec <prestataire.0197512@ouest-france.fr>
2024-06-05 12:25:27 +0200

    feat: Add filter by type

--- src/views/MapDetails.tsx
+++ src/views/MapDetails.tsx
@@ -5,12 +5,14 @@ import { useMap } from "../hooks/useMap";
 import { useAgents } from "../hooks/useAgents";
 import { useAgentScorePerMap } from "../hooks/useAgentScorePerMap";
 import { AgentStats } from "../components/AgentStats/AgentStats";
+import { useState } from "react";
 
 export function MapDetails() {
   const { id } = useParams();
   const { data } = useMap(id as string);
   const { agents } = useAgents();
   const { scores } = useAgentScorePerMap(data.displayName?.toLowerCase());
+  const [typeFilter, setTypeFilter] = useState("all");
 
   return (
     <Page>
@@ -18,20 +20,42 @@ export function MapDetails() {
 
       <img style={{ width: "80vw" }} src={data.splash} alt={data.displayName} />
 
+      <label htmlFor="select-type">Choix type</label>
+      <select
+        id="select-type"
+        value={typeFilter}
+        onChange={(evt) => setTypeFilter(evt.currentTarget.value)}
+      >
+        <option value="all">Tous</option>
+        {[...new Set(agents.map((agent) => agent.role.displayName))].map(
+          (type) => (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          )
+        )}
+      </select>
+
       <>
-        {scores.map((score) => (
-          <AgentStats
-            key={score.playerName}
-            score={score}
-            agent={
-              agents.find(
-                (elt) =>
-                  elt.displayName.toLowerCase() ===
-                  score.playerName.toLowerCase()
-              )!
-            }
-          />
-        ))}
+        {scores.length > 0 &&
+          agents
+            .filter(
+              (elt) =>
+                elt.role.displayName === typeFilter || typeFilter === "all"
+            )
+            .map((agent) => (
+              <AgentStats
+                key={agent.uuid}
+                score={
+                  scores.find(
+                    (elt) =>
+                      elt.playerName.toLowerCase() ===
+                      agent.displayName.toLowerCase()
+                  )!
+                }
+                agent={agent}
+              />
+            ))}
       </>
     </Page>
   );
